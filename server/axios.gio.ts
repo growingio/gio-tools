a5b45d86140ea2a2545b5fc7a597044410fefd2c
IceMimosa <chk19940609@gmail.com>
2018-11-26 21:29:25 +0800

    Fix axios

---
+++ server/axios.gio.ts
@@ -0,0 +1,18 @@
+import axios from 'axios-https-proxy-fix';
+
+function handle() {
+  const proxy = process.env.http_proxy;
+  if (!!proxy) {
+    const url = require('url');
+    const p = url.parse(proxy);
+    return axios.create({
+      proxy: {
+        host: p.hostname,
+        port: p.port,
+      },
+    });
+  }
+  return axios;
+}
+
+export default handle();
